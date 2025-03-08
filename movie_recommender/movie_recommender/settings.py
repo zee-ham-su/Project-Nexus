5a9f9faa5467755e56b408dcf37b595c42667df7
zee-ham-su <hsufiian@yahoo.com>
2025-03-08 10:13:38 +0000

    feat: refactor database settings to use dj-database-url for improved configuration and add dj-database-url dependency

--- movie_recommender/movie_recommender/settings.py
+++ movie_recommender/movie_recommender/settings.py
@@ -15,6 +15,7 @@ import os
 from dotenv import load_dotenv
 from datetime import timedelta
 import logging
+import dj_database_url
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -92,14 +93,7 @@ WSGI_APPLICATION = 'movie_recommender.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
