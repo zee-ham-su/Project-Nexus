1d4d0a5a7a59ad3637cf77a676afe18d6cd12d46
zee-ham-su <hsufiian@yahoo.com>
2025-03-11 18:03:11 +0000

    fix:  Fix incorrect token refresh view in URL config

--- movie_recommender/users/urls.py
+++ movie_recommender/users/urls.py
@@ -10,7 +10,9 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token-obtain-pair'),
-    path('token/refresh/', CustomTokenObtainPairView.as_view(), name='token-refresh'),
-    path('favorites/', FavoriteMoviesView.as_view(), name='auth-favorites-list-create'),
-    path('favorites/<int:id>/', FavoriteMovieDetailView.as_view(), name='auth-favorites-delete'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('favorites/', FavoriteMoviesView.as_view(),
+         name='auth-favorites-list-create'),
+    path('favorites/<int:id>/', FavoriteMovieDetailView.as_view(),
+         name='auth-favorites-delete'),
 ]
